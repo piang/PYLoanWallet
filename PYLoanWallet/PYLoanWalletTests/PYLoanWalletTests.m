4ec203a70e45fd40acd629cf51bb8b5e8a154672
peiyang <peiyang@9fbank.cc>
2017-08-27 21:48:44 +0800

    新建项目

---
+++ PYLoanWallet/PYLoanWalletTests/PYLoanWalletTests.m
@@ -0,0 +1,39 @@
+//
+//  PYLoanWalletTests.m
+//  PYLoanWalletTests
+//
+//  Created by 洋 裴 on 2017/8/27.
+//  Copyright © 2017年 piang. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface PYLoanWalletTests : XCTestCase
+
+@end
+
+@implementation PYLoanWalletTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
